eda99a6c0f6c257a0725238a33e006efa3440ae5
suint <kellymi@udel.edu>
2020-12-19 00:14:30 -0500

    lol

--- docs/ctf/script.js
+++ docs/ctf/script.js
@@ -2,3 +2,4 @@ function hideDescription() {
     document.getElementById("description").style.display = "none";
     document.getElementById("challenge").style.display = "block";
 }
+document.cookie = "isAdmin=ZmFsc2U";
